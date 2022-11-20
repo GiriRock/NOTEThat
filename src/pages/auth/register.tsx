cf4b464f779f24f77cd29a617fdc4ebd5f7dd4fa
girirock <giridharanrock@gmail.com>
2022-11-20 13:44:25 +0530

    register link tag updated

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -144,12 +144,12 @@ const Container: React.FC = () => {
                                 className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                             />
                         </div>
-                        <a
+                        <Link
                             href=""
                             className="text-xs text-purple-600 hover:underline"
                         >
                             Forget Password?
-                        </a>
+                        </Link>
                         <div className="mt-6">
                             <button type='submit'
                                 className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
