ba82144098bafcf9d19212a73a0ebeb915afa480
girirock <giridharanrock@gmail.com>
2022-11-20 12:36:44 +0530

    login

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -87,8 +87,8 @@ const Container : React.FC = () => {
                 </form>
 
                 <p className="mt-8 text-xs font-light text-center text-gray-700">
-                    {` `}
-                    Don't have an account?{` `}
+                &nbsp;
+                    Don't have an account?&nbsp;
                     <a
                         href="#"
                         className="font-medium text-purple-600 hover:underline"
