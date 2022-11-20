d13e47d615fa32d402dc62dff722ff62f78de35d
girirock <giridharanrock@gmail.com>
2022-11-20 15:32:01 +0530

    bug fix

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
 import { ThreeDots } from 'react-loader-spinner';
@@ -95,12 +96,12 @@ const Container: React.FC = () => {
                                 className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                             />
                         </div>
-                        <a
+                        <Link
                             href=""
                             className="text-xs text-purple-600 hover:underline"
                         >
                             Forget Password?
-                        </a>
+                        </Link>
                         <div className="mt-6">
                             <button type='submit'
                                 className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
